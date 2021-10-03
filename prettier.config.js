91be4713b72d9bd2b16568037c8378dc86562485
Wésley Queiroz <wesley.coder@gmail.com>
2021-10-03 18:26:06 -0300

    chore(config): migrating project file to hxp format

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: "es5",
+  bracketSpacing: true,
+  arrowParens: "avoid",
+  printWidth: 120,
+  proseWrap: "always",
+  endOfLine: "lf",
+};
